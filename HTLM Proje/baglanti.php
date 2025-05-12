50a5c2321e1717deff1b4c3a9f09c1409649174c
Ömer Faruk Sarı <omerfaruksari0077@gmail.com>
2025-05-12 18:07:39 +0300

    Update baglanti.php

--- HTLM Proje/baglanti.php
+++ HTLM Proje/baglanti.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $kullanici = "root";
 $parola = "";
-$vt = "ilanlar";
+$vt = "realestate";
 
 $baglanti = mysqli_connect($host, $kullanici, $parola, $vt);
 mysqli_set_charset($baglanti, "UTF8");
