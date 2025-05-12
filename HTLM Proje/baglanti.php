219f987d55f394add5b7cacd26f17de04f20cfac
Ömer Faruk Sarı <omerfaruksari0077@gmail.com>
2025-05-12 17:20:43 +0300

    Add files via upload

---
+++ HTLM Proje/baglanti.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$kullanici = "root";
+$parola = "";
+$vt = "ilanlar";
+
+$baglanti = mysqli_connect($host, $kullanici, $parola, $vt);
+mysqli_set_charset($baglanti, "UTF8");
+
+
+?>
+<!-- bu dosya uyelik db'ne ulaşmak için -->
